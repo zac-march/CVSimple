a316c56ea0eeece0f6dac5dde90fc894b7af4cc7
Zac <121649297+zac-march@users.noreply.github.com>
2023-04-27 08:17:47 +1000

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Resume Builder</h1>
+        <h2>An application by Zac March</h2>
+      </header>
+    );
+  }
+}
+
+export default Header;
