715af02059e6fd0508bf340942fc63af4d037b20
Zac <121649297+zac-march@users.noreply.github.com>
2023-04-27 12:51:53 +1000

    Add preview component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import Preview from "./components/Preview";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
+      <Preview />
     </div>
   );
 }
---
+++ src/components/Preview.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Preview extends React.Component {
+  render() {
+    return <div>preview</div>;
+  }
+}
+
+export default Preview;
